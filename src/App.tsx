663ba5b3c0db38c79a78df26f46bc414db1f69c8
Ellis <onlyheart@qq.com>
2022-04-23 12:19:35 +0800

    add mui & styleguide

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained">Test</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
