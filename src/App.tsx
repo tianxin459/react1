3ce216e75ca0d40afd040a192583a0a83bc3ce28
Ellis <onlyheart@qq.com>
2022-04-23 23:20:01 +0800

    sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,57 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Box, Button, Divider, List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer } from "@mui/material";
+import { Fragment, useState } from "react";
+import { BrowserRouter, Route, Routes, useLocation, NavLink } from "react-router-dom";
 import style from "./App.module.scss";
 import MasterDataContainer from "./container/MasterDataContainer";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 const About = () => {
   // 使用 hook
   const location = useLocation();
-  const { search, pathname } = location;
+  const { pathname } = location;
 
   return (
-    <div>
-      router {pathname}，search= {search}
-    </div>
+    <Fragment>
+      <div>router {pathname}</div>
+    </Fragment>
   );
 };
 
 let App = () => {
-  const [token, setToken] = useState<string>("");
+  const [sideNav, setSideNav] = useState<boolean>(false);
 
-  const fakeAuth = (): Promise<string> =>
-    new Promise<string>((resolve) => {
-      setTimeout(() => resolve("2342f2f1d131rf12"), 250);
-    });
-
-  const handleLogin = async () => {
-    const token = await fakeAuth();
-    setToken(token);
+  const sideNavContent = () => (
+    <div style={{ width: "auto" }} role="presentation" onClick={onClickHandler} onKeyDown={onClickHandler}>
+      <List>
+        {["/", "/masterdata", "/about"].map((path, index) => (
+          <ListItem key={path} component={NavLink} to={path}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={path} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  const onClickHandler = () => {
+    console.log("click");
+    setSideNav(!sideNav);
   };
+
   return (
     <div className={style.App}>
-      <div>Header</div>
+      <Button onClick={() => onClickHandler()}>SideNav</Button>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MasterDataContainer onLogin={handleLogin} />} />
+          <Route path="/" element={<MasterDataContainer />} />
           <Route path="/masterdata" element={<MasterDataContainer />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <SwipeableDrawer anchor={"left"} open={sideNav} onClose={onClickHandler} onOpen={onClickHandler}>
+          {sideNavContent()}
+        </SwipeableDrawer>
       </BrowserRouter>
     </div>
-    // <div className={style.App}>
-    //   <header className="App-header">
-    //     <p>this is App header</p>
-    //   </header>
-    //   <MasterDataContainer></MasterDataContainer>
-    // </div>
   );
 };
 
