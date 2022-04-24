e3f6b468c1e15fa7a4f18f45b21fc34f2a19caed
Ellis <onlyheart@qq.com>
2022-04-24 14:53:46 +0800

    app refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
-import { Box, Button, Divider, List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer } from "@mui/material";
+import { AppBar, Box, Button, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer, Toolbar } from "@mui/material";
 import { Fragment, useState } from "react";
 import { BrowserRouter, Route, Routes, useLocation, NavLink } from "react-router-dom";
 import style from "./App.module.scss";
 import MasterDataContainer from "./container/MasterDataContainer";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
 import Counter from "./components/Counter/Counter";
+import About from "./components/About/About";
 
 const RouterConfig = [
-  { path: "/", name: "Home", component: () => <MasterDataContainer></MasterDataContainer> },
-  { path: "/counter", name: "Counter", component: () => <Counter></Counter> },
-  { path: "/about", name: "About", component: () => <div>About</div> },
-  { path: "/masterdata", name: "MasterData", component: () => <MasterDataContainer></MasterDataContainer> },
+  { path: "/", name: "Home", component: <MasterDataContainer></MasterDataContainer> },
+  { path: "/counter", name: "Counter", component: <Counter></Counter> },
+  { path: "/about", name: "About", component: <About></About> },
+  { path: "/masterdata", name: "MasterData", component: <MasterDataContainer></MasterDataContainer> },
 ];
-
-const About = () => {
-  // 使用 hook
-  const location = useLocation();
-  const { pathname } = location;
-
-  return (
-    <Fragment>
-      <div>router {pathname}</div>
-    </Fragment>
-  );
-};
 
 let App = () => {
   const [sideNav, setSideNav] = useState<boolean>(false);
@@ -32,12 +21,17 @@ let App = () => {
   const sideNavContent = () => (
     <div style={{ width: "auto" }} role="presentation" onClick={onClickHandler} onKeyDown={onClickHandler}>
       <List>
-        {["/", "/masterdata", "/counter", "/about"].map((path, index) => (
+        {RouterConfig.map((config) => (
+          <ListItem key={config.path} component={NavLink} to={config.path}>
+            <ListItemText primary={config.path} />
+          </ListItem>
+        ))}
+        {/* {["/", "/masterdata", "/counter", "/about"].map((path, index) => (
           <ListItem key={path} component={NavLink} to={path}>
             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
             <ListItemText primary={path} />
           </ListItem>
-        ))}
+        ))} */}
       </List>
     </div>
   );
@@ -48,15 +42,19 @@ let App = () => {
 
   return (
     <div className={style.App}>
-      <Button onClick={() => onClickHandler()}>SideNav</Button>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => onClickHandler()}>
+            <MenuIcon />
+          </IconButton>
+          <Button onClick={() => onClickHandler()}>SideNav</Button>
+        </Toolbar>
+      </AppBar>
       <BrowserRouter>
         <Routes>
           {RouterConfig.map((config) => (
-            <Route path={config.path} element={config.component()} />
+            <Route path={config.path} element={config.component} />
           ))}
-          {/* <Route path="/masterdata" element={<MasterDataContainer />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/about" element={<About />} /> */}
         </Routes>
         <SwipeableDrawer anchor={"left"} open={sideNav} onClose={onClickHandler} onOpen={onClickHandler}>
           {sideNavContent()}
